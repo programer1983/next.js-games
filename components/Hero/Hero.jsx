e9c160f68569b61380d65c697008ed53a252ffb1
Prodi <programer972@gmail.com>
2026-09-12 15:22:17 +0300

    update hero section

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,56 +1,52 @@
-'use client';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import Image from 'next/image'
-import'./Hero.scss'
-import { heroImages } from '../../data'
-import { Navigation, Pagination} from 'swiper/modules';
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import 'swiper/css/navigation';
+import Image from "next/image";
+import "./Hero.scss";
+import { heroImages } from "../../data";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export default function Hero() {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 768px)'); 
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
     setIsMobile(mediaQuery.matches);
 
     const handleResize = () => {
       setIsMobile(mediaQuery.matches);
     };
 
-    mediaQuery.addEventListener('change', handleResize);
-    return () => mediaQuery.removeEventListener('change', handleResize);
+    mediaQuery.addEventListener("change", handleResize);
+    return () => mediaQuery.removeEventListener("change", handleResize);
   }, []);
 
-
   return (
-    <section className='hero' id="hero">
-        <Swiper
-            loop={true}
-            slidesPerView={1}
-            speed={800}
-            navigation
-            pagination={isMobile ? { clickable: true } : false} 
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-            modules={[Navigation, Pagination]}
-        >
-            {heroImages.map((image) => (
-            <SwiperSlide
-               key={image.id}
-               className='hero__slide-item'
-            >
-            <img 
-                className='hero__slide-image'
-                src={image.imageUrl} 
-                alt={image.alt} 
+    <section className="hero" id="hero">
+      <Swiper
+        loop={true}
+        slidesPerView={1}
+        speed={800}
+        navigation
+        pagination={isMobile ? { clickable: true } : false}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        modules={[Navigation, Pagination]}
+      >
+        {heroImages.map((image) => (
+          <SwiperSlide key={image.id} className="hero__slide-item">
+            <img
+              className="hero__slide-image"
+              src={image.imageUrl}
+              alt={image.alt}
             />
-            </SwiperSlide>
-            ))}
-       </Swiper>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
-  )
+  );
 }
